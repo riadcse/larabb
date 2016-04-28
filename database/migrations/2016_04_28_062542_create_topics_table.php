57460c5c0c02a22ae8e75e58100169103779fdec
Jason Clemons <zilladotexe@gmail.com>
2016-04-28 02:31:50 -0400

    Added category, board, topic, reply migrations

---
+++ database/migrations/2016_04_28_062542_create_topics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject', 150);
+            $table->text('body');
+            $table->integer('id_author');
+            $table->integer('id_board');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('topics');
+    }
+}
